e97b6afb124478a5097dc135db9782f07006b84b
Oleh Tulaidan <oleh.tulaidan@gmail.com>
2022-09-05 18:41:13 +0300

    Сonnected styles of the basicLightbox modal window library

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryImg = galleryItems
+  .map((item) => `<li><img src=${item.preview} alt="${item.description}"></li>`)
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", `<ul>${galleryImg}</ul>`);
+
+gallery.addEventListener("click", openModal);
+
+function openModal(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(`
+    <img src="${galleryItems[0].original}" width="1280" height="600">
+`);
+
+  instance.show();
+  console.log(event);
+}
