d5dba615fb977decfd120f5568a1608474085ba3
Oleh Tulaidan <oleh.tulaidan@gmail.com>
2022-10-03 20:01:15 +0300

    Add modal whith simplelightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryCreating = (galleryItems) => {
+  return galleryItems
+    .map(
+      ({ original, preview, description }) =>
+        `<div class="gallery__item"> 
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}"
+            data-source="${original}" alt="${description}"/>
+        </a>
+    </div>`
+    )
+    .join("");
+};
+
+gallery.insertAdjacentHTML("beforeend", galleryCreating(galleryItems));
+
+gallery.insertAdjacentHTML("beforeend", galleryCreating(galleryItems));
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionType: `attr`,
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
